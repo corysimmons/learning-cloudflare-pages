e57302fe0926520a689a5cfe3237ac7a50c8df81
Cory Simmons <csimmonswork@gmail.com>
2023-10-18 14:38:07 -0400

    Try the workers-og example

--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<svelte:head>
-	<meta property="og:image" content="http://localhost:5173/api/og" />
-</svelte:head>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
